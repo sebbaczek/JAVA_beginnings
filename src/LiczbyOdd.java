34d511cf2930b4af8b20e72a69c15f0abb194d66
DESKTOP-BFV5RNS\Sebastian <sebastian451@interia.pl>
2022-08-02 14:15:17 +0200

    Initial commit

---
+++ src/LiczbyOdd.java
@@ -0,0 +1,10 @@
+public class LiczbyOdd {
+    public static void main(String[] args) {
+        int x=556;
+        if (x%2==0){
+            System.out.println(x+"  jest parzysta");
+        } else {
+            System.out.println(x+" nie jest parzysta");
+        }
+    }
+}

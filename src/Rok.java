2f1a8f3cd456189b77991a20c5f0eeff2cc3a43d
DESKTOP-BFV5RNS\Sebastian <sebastian451@interia.pl>
2022-08-02 15:38:55 +0200

    Initial commit

--- src/Rok.java
+++ src/Rok.java
@@ -1,7 +1,7 @@
 public class Rok {
     public static void main(String[] args) {
-        int rok =2024;
-        if ((rok%4==0)&& !(rok%100==0)){
+        int rok =2000;
+        if ((rok%4==0)&& (!(rok%100==0)|| !(rok%400==0))){
             System.out.println("jest przestêpny");
         } else {
             System.out.println("nie jest przestêpny");

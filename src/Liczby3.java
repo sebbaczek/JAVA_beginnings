92dc8905be4f6ca048634f1608dd1f6661164133
DESKTOP-BFV5RNS\Sebastian <sebastian451@interia.pl>
2022-08-02 14:09:34 +0200

    Initial commit

---
+++ src/Liczby3.java
@@ -0,0 +1,12 @@
+public class Liczby3 {
+    public static void main(String[] args) {
+        int x=3,y=40,z=5;
+        if ((x>y)&&(x>z)){
+            System.out.println("x jest najwiêksze");
+        } else if (((y>x)&&(y>z))) {
+            System.out.println("y jest najwiêksze");
+        } else {
+            System.out.println("z jest najwiêksze");
+        }
+    }
+}

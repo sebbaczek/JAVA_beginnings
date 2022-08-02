4d4b8d99e6270da3d6aa48c702b5e74f396761b4
DESKTOP-BFV5RNS\Sebastian <sebastian451@interia.pl>
2022-08-02 10:12:30 +0200

    Initial commit

--- src/Byte.java
+++ src/Byte.java
@@ -1,13 +1,13 @@
 public class Byte {
     public static void main(String[] args) {
-        var y = 0;
-        long v = 0;var z = 1.2;
+
         byte x = 0;
-        ++x;
-        y = (int) (x+x);
-        v =(long) (y+y);
-        z = (float) (x/2*v);
-        System.out.println(x+" "+y+" "+v+" "+z+"");
+        int a = x+0;
+        long b = x + 2L;
+        float c = x +4.2f;
+        double d = x+5.1;
+
+        System.out.println(x+" "+a+" "+b+" "+c+" "+d);
     }
 
 

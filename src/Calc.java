5e5753be6d6a52268272f62ddc298d8fb063a5f2
DESKTOP-BFV5RNS\Sebastian <sebastian451@interia.pl>
2022-08-02 09:13:23 +0200

    Initial commit

---
+++ src/Calc.java
@@ -0,0 +1,15 @@
+public class Calc {
+    public static void main(String[] args) {
+
+        double a = 3.3,b = 5.5;
+        calc(a,b);
+    }
+
+   public static void calc(double x, double y) {
+        System.out.println(x+y);
+        System.out.println(x*y);
+        System.out.println(x-y);
+        System.out.println(x/y);
+
+    }
+}

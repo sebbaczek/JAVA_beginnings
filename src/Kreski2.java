f9929db9a47028ff58412e7b4472fe1cf2106665
DESKTOP-BFV5RNS\Sebastian <sebastian451@interia.pl>
2022-08-03 08:10:57 +0200

    Initial commit

---
+++ src/Kreski2.java
@@ -0,0 +1,26 @@
+public class Kreski2 {
+    public static void main(String[] args) {
+        int x =100;
+//        int red = 1, green = 2, blue = 3, black = 4, yellow = 5, brown = 6;
+for (int i =0;i<x;i++){
+    if ((i+1)%6==0){
+        System.out.println("brown");
+    }if ((i+1)%6==1){
+        System.out.println("red");
+    }if ((i+1)%6==2){
+        System.out.println("green");
+    }if ((i+1)%6==3){
+        System.out.println("blue");
+    }if ((i+1)%6==4){
+        System.out.println("black");
+    }if ((i+1)%6==5){
+        System.out.println("yellow");
+    }
+
+
+}
+
+    }
+
+
+}

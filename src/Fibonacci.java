0312f56a4ef7efe9ae4aefd3dd48a00bb1974afc
DESKTOP-BFV5RNS\Sebastian <sebastian451@interia.pl>
2022-08-03 08:59:35 +0200

    Initial commit

---
+++ src/Fibonacci.java
@@ -0,0 +1,14 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        int x = 0;
+        int y = 1;
+        int v;
+        int z = 20;
+        for (int i = 0; i < z; i++) {
+            System.out.print(x + " ");
+            v = y + x;
+            x = y;
+            y = v;
+        }
+    }
+}

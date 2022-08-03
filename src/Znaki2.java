cbf5b3bd6e9efd65ac7fe9d109c8900549fdebef
DESKTOP-BFV5RNS\Sebastian <sebastian451@interia.pl>
2022-08-03 09:07:34 +0200

    Initial commit

---
+++ src/Znaki2.java
@@ -0,0 +1,12 @@
+public class Znaki2 {
+    public static void main(String[] args) {
+int x =9;
+int y =4;
+for(int i =1;i<=x;i++){
+    System.out.println(i+""+i+""+i);
+    for(int j =1;j<=y;j++){
+        System.out.println("####");
+    }
+}
+    }
+}

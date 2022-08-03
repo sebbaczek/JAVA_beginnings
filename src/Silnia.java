e6ae03d1072888544f7e5c562889d16dad6dc78d
DESKTOP-BFV5RNS\Sebastian <sebastian451@interia.pl>
2022-08-03 13:54:22 +0200

    Initial commit

--- src/Silnia.java
+++ src/Silnia.java
@@ -1,10 +1,23 @@
 public class Silnia {
-    public static void main(String[] args) {
+      public static void main(String[] args) {
+        System.out.println(silnia(6));
         int x=1,y=0;
         for(int i =1;i<20;i++){
              y = x*i;
              x = y;
             System.out.println("silnia "+i+" wynosi "+y);
+
+        }}
+
+        private static int silnia (int n){
+            int wynik =1;
+            for (int i = 1; i <=n ; i++) {
+                wynik = wynik*i;
+                
+            }
+            return wynik;
         }
+
+
     }
-}
+

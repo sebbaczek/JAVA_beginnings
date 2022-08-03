9cb290f7f3272f884f036ce8f8e1a42272422f9f
DESKTOP-BFV5RNS\Sebastian <sebastian451@interia.pl>
2022-08-03 18:07:50 +0200

    liczby pierwsze update

--- src/Pierwsze.java
+++ src/Pierwsze.java
@@ -1,11 +1,15 @@
 public class Pierwsze {
     public static void main(String[] args) {
-        for (int i = 2; i <1000 ; i++) {
-            for (int j = 2; j <i ; j++) {
+        System.out.println(2);
+        System.out.println(3);
+        for (int i = 2; i <10000 ; i++) {
+            for (int j = 2; j <=i/2 ; j++) {
+
                if (i%j==0){
                     break;
                } else {
-                   if (j !=i-1){
+
+                   if (j !=i/2){
                        continue;
                    }else {
                        System.out.println(i);

1a307292db92243ea96c24c8355c553852c3f8c9
DESKTOP-BFV5RNS\Sebastian <sebastian451@interia.pl>
2022-08-04 11:02:34 +0200

    metoda odwraca tablicę

--- src/Rev.java
+++ src/Rev.java
@@ -1,9 +1,23 @@
+import java.util.Arrays;
+
 public class Rev {
     public static void main(String[] args) {
         String s = "Sebastian5";
+        int[] arr={5,7,9,10,11,14,16};
         reverse(s);
+        revarr(arr);
     }
-/** odwraca s³owo*/
+    /** odwraca tablicê*/
+    private static void revarr(int[] arr) {
+        int[] arr2 = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            arr2[arr.length-1-i] = arr[i];
+        }
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr2));
+    }
+
+    /** odwraca s³owo*/
     private static void reverse(String s) {
         String rev = "";
         for (int i = s.length()-1; i >=0; i--) {

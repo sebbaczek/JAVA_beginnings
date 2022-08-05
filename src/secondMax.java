09772e0c307285cd9bd761b85e7a06fee5c0f419
DESKTOP-BFV5RNS\Sebastian <sebastian451@interia.pl>
2022-08-05 17:27:50 +0200

    usuwa duplikaty na końcu

--- src/secondMax.java
+++ src/secondMax.java
@@ -3,19 +3,28 @@ import java.util.Arrays;
 /**Napisz program, który znajdzie druga najwiêksz¹ wartoœæ w tablicy*/
  public class secondMax {
     public static void main(String[] args) {
-    int[] arr = {34,35,-7,6,78,-6,158,58,76,-45,23,4,5,3,660,555,777,4,-7,6,78,-6,158,58,76,-45};
+    int[] arr = {34,35,-7,6,78,-6,158,58,76,-45,23,4,5,3,660,555,777,777,4,-7,6,78,-6,158,58,76,-45};
        int result = find2Max(arr);
         find2Max2method(arr);
         System.out.println(result);
     }
-
+    /**w posortowanej tablicy trzeba sprawdziæ czy najwiêksza liczba siê nie duplikuje*/
     private static void find2Max2method(int[] arr) {
         Arrays.sort(arr);
-        System.out.println(arr[arr.length-2]);
+        int max2 =0;
+        if(arr[arr.length-2]==arr[arr.length-1]){
+            for (int i = arr.length-2; i >=0 ; i--) {
+                if (arr[i]<arr[arr.length-1]){
+                    max2 = arr[i];
+                    break;
+                }
+            }
+        } else { max2 = arr[arr.length-2];}
+        System.out.println(max2);
     }
 
     private static int find2Max(int[] arr) {
-        int max2;
+        int max2 = 0;
         for (int j = 0; j < arr.length; j++) {
             int elem = arr[j];
            if (elem>arr[arr.length-2]) {
@@ -33,7 +42,17 @@ import java.util.Arrays;
                 arr[arr.length-2] = last;
             }
         }
-        max2 = arr[arr.length-2];
+        Arrays.sort(arr);
+        if(arr[arr.length-2]==arr[arr.length-1]){
+            for (int i = arr.length-2; i >=0 ; i--) {
+                if (arr[i]<arr[arr.length-1]){
+                    max2 = arr[i];
+                    break;
+                }
+            }
+        } else { max2 = arr[arr.length-2];}
+
+
         System.out.println(Arrays.toString(arr));
         return max2;
     }
